f0faa3533442294ff8d7900aef7b817f8c77c86b
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2023-02-02 17:12:06 -0300

    Tipos var

---
+++ introducao_dart/lib/a_variaveis/b_tipos_variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Declarações
+  var curso = 'Academia do Flutter';
+  var acessoEmAnos = 2;
+  var valor = 1196.00;
+  var turmaAberta = false;
+
+  // Tipos nulos
+  // Todos os tipos podem receber 2 valores (NonNull e Null)
+  String? cursoNulo;
+  int? acessoEmAnosN;
+  double? valorN;
+  bool? turmaAbertaN;
+}
