1ff3b114056bc79f55b6f6cd1ecdc3a403f64a8b
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2023-02-02 13:07:44 -0300

    Printando variáveis.

--- introducao_dart/lib/a_variaveis/a_tipos_variaveis.dart
+++ introducao_dart/lib/a_variaveis/a_tipos_variaveis.dart
@@ -1,4 +1,28 @@
 void main() {
-  //Tipos
-  //String, int, double, bool, num
+  // Tipos
+  // String, int, double, bool, num
+
+  // Declarações
+  String curso = 'Academia do Flutter';
+  int acessoEmAnos = 2;
+  double valor = 1196.00;
+  bool turmaAberta = false;
+
+  // Tipos nulos
+  // Todos os tipos podem receber 2 valores (NonNull e Null)
+  String? cursoNulo;
+  int? acessoEmAnosN;
+  double? valorN;
+  bool? turmaAbertaN;
+
+  print([
+    curso,
+    acessoEmAnos,
+    valor,
+    turmaAberta,
+    cursoNulo,
+    acessoEmAnosN,
+    valorN,
+    turmaAbertaN
+  ]);
 }
