409beb29a1bde9010815e47f70517b0e8ea43059
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2023-02-04 19:35:59 -0300

    Introdução ao Flutter

--- introducao_dart/bin/introducao_dart.dart
+++ introducao_dart/bin/introducao_dart.dart
@@ -1,5 +1,3 @@
 import 'package:introducao_dart/introducao_dart.dart' as introducao_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${introducao_dart.calculate()}!');
-}
+void main(List<String> arguments) {}
--- introducao_flutter/lib/main.dart
+++ introducao_flutter/lib/main.dart
@@ -13,16 +13,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.red,
+        ),
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(appBar: AppBar()),
+    );
+  }
+}
+
+/*class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -96,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você deve pushed the button this many times:',
             ),
             Text(
               '$_counter',
@@ -112,4 +122,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+} */
