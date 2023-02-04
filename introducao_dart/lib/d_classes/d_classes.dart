b3ae48ac2aec76eccb3135a91d3bf604d373cfb0
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2023-02-03 21:27:13 -0300

    Classes no Dart

---
+++ introducao_dart/lib/d_classes/d_classes.dart
@@ -0,0 +1,28 @@
+void main() {
+  var camisetaADF = Camiseta();
+  camisetaADF.cor = 'Branca';
+  camisetaADF.tamanho = 'M';
+  camisetaADF.marca = 'Academia do Flutter';
+  camisetaADF.modelo = 'Gola Careca';
+
+  print('A cor da camiseta da ${camisetaADF.marca} é ${camisetaADF.cor}');
+  print(
+      'O tipo de lavagem da camiseta da ${camisetaADF.marca} é ${camisetaADF.tipoDeLavagem()}');
+}
+
+class Camiseta {
+  // características
+  String? cor;
+  String? tamanho;
+  String? marca;
+  String? modelo;
+
+  // comportamentos
+  String tipoDeLavagem() {
+    if (marca == 'Academia do Flutter') {
+      return 'Pode lavar na máquina';
+    } else {
+      return 'Não pode lavar na máquina';
+    }
+  }
+}
