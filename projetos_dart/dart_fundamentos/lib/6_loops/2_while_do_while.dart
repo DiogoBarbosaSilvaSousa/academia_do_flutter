05edde8bfef6395fcc3fad7de0dc2d2f84c99c3f
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2023-05-29 19:24:37 -0300

    While DoWhile

---
+++ projetos_dart/dart_fundamentos/lib/6_loops/2_while_do_while.dart
@@ -0,0 +1,15 @@
+void main() {
+  var numero = 0;
+  print('\nWhile Convencional\n');
+  while (numero < 10) {
+    print(numero);
+    numero++;
+  }
+
+  var indice = 0;
+  print('\nDoWhile\n');
+
+  do {
+    print(indice);
+  } while (indice > 0);
+}
