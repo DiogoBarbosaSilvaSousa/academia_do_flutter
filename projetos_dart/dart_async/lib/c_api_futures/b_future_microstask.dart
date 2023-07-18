566a9d6b955d9c5aaa65950a28350815bfa12bc6
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2023-07-18 00:05:04 -0300

    Future Microtask

---
+++ projetos_dart/dart_async/lib/c_api_futures/b_future_microstask.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('Init Main');
+
+  Future<String>(() => 'Future Normal').then(print);
+  Future<String>.microtask(() => Future.error('Erro ao executar'))
+      .then(print)
+      .catchError(print);
+
+  print('End Main');
+}
