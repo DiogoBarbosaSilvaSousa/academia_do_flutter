9935c62723c094768d87390ec27846b9740fa9d6
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2023-07-30 22:49:45 -0300

    Dart lab Academia do Flutter

---
+++ projetos_dart/dart_lab_adf/adf_cli/bin/adf_cli.dart
@@ -0,0 +1,3 @@
+import 'package:adf_cli/adf_cli.dart' as adf_cli;
+
+void main(List<String> arguments) {}
---
+++ projetos_dart/dart_lab_adf/adf_cli/test/adf_cli_test.dart
@@ -0,0 +1,6 @@
+import 'package:adf_cli/adf_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {});
+}
